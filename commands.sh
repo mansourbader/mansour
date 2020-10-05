55d149628c36426a8601fb33f47ae90156c0ead3
mansourbader <61111442+mansourbader@users.noreply.github.com>
2020-10-05 11:17:29 +0300

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,3 +1,3 @@
-wget http://nightly.odoo.com/7.0/nightly/deb/openerp_7.0-latest-1_all.deb
+wget http://nightly.odoo.com/7.0/nightly/deb/openerp_7.0.latest_all.deb
 sudo dpkg -i openerp_7.0-latest-1_all.deb
 sudo apt-get install -f
